2e8c73ff9ac55ee712ae9b3018202563158a60f0
csu1yi <aroncsulyak@gmail.com>
2022-09-10 13:45:59 +0200

    errorjavitasok

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //hiányzik az "_" az NELEMENTS-ből
-    std::cout << '1-100 ertekek duplazasa' // string-eket " -al használjuk, nem pedig ' -el, illetve hiányzik a pontosvessző (;)
-    for (int i = 0;) //hiányzó feltétel, lépésköz és a végén nincs pontosvessző (;)
+    int *b = new int[N_ELEMENTS]; //hiányzik az "_" az NELEMENTS-ből
+    std::cout << "1-100 ertekek duplazasa"<<std::endl; // string-eket " -al használjuk, nem pedig ' -el, illetve hiányzik a pontosvessző (;)
+    for (int i = 0; i<N_ELEMENTS; i++) //hiányzó feltétel, lépésköz és a végén nincs pontosvessző (;)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //nem elegendő feltétel (i<N_ELEMENTS)
+    for (int i = 0; i<N_ELEMENTS; i++) //nem elegendő feltétel (i<N_ELEMENTS)
     {
-        std::cout << "Ertek:"  //kiírja hogy érték, de azt nem, hogy mi az az érték
+        std::cout << "Ertek: " <<b[i]<<std::endl; //kiírja hogy érték, de azt nem, hogy mi az az érték
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // az átlag nem egy egész szám (double)
-    for (int i = 0; i < N_ELEMENTS, i++) // a második vessző helyére pontos vessző kell
+    double atlag=0; // az átlag nem egy egész szám (double)
+    for (int i = 0; i < N_ELEMENTS; i++) // a második vessző helyére pontos vessző kell
     {
-        atlag += b[i] //hiányzó pontosvessző,
+        atlag += b[i]; //hiányzó pontosvessző,
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
