13e930dcb83a51c8241d91a0bbfb30a0b121a7df
csu1yi <aroncsulyak@gmail.com>
2022-09-10 13:32:57 +0200

    errorcomments

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hiányzik az "_" az NELEMENTS-ből
+    std::cout << '1-100 ertekek duplazasa' // string-eket " -al használjuk, nem pedig ' -el, illetve hiányzik a pontosvessző (;)
+    for (int i = 0;) //hiányzó feltétel, lépésköz és a végén nincs pontosvessző (;)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //nem elegendő feltétel (i<N_ELEMENTS)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"  //kiírja hogy érték, de azt nem, hogy mi az az érték
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // az átlag nem egy egész szám (double)
+    for (int i = 0; i < N_ELEMENTS, i++) // a második vessző helyére pontos vessző kell
     {
-        atlag += b[i]
+        atlag += b[i] //hiányzó pontosvessző,
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
